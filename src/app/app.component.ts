2121935ec06e5f39120b5b7379a41e4e514abfe1
ivanroscuenca <ivanros@protonmail.com>
2022-12-26 19:42:18 +0100

    Added index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,23 @@ export class AppComponent {
   registrado=false;
   nombre:string="";
   apellido:string="";
+  entradas:any;
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada día mas presente"},
+     {titulo:"Java presente desde hace más de 20 años"},
+     {titulo:"Javascript cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"Pascal, cada vez menos utilizado"},
+    ]
+  };
+
 
   registrarUsuario(){
 
     this.registrado=true;
     this.mensaje = "Usuario registrado con éxito";
 
-  }
+  };
 }
+
